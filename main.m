f010d4f1354a5fe1c7e25f95fbb4859e615591b2
Jaime de Salas <jaimedesalasjuanas@gmail.com>
2026-01-02 12:37:21 +0100

    added main file comments

---
+++ main.m
@@ -0,0 +1,8 @@
+%% Evaluate loads
+% L, M
+%% Evaluate structures
+% Wing_weight
+%% Evaluate aero
+% L, D
+%% Evaluate fuel tank volume (performance)
+%% Evaluate Range using calculatePerformance.m
