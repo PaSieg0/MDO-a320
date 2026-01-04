b182fdc095cd80bd813823e9928c3c1e336e9f30
Mathiaskenneth <igarridopueyo@tudelft.nl>
2026-01-04 19:26:36 +0100

    Bug fuel tank calc in perf

--- performance.m
+++ performance.m
@@ -24,7 +24,7 @@ filename = 'optimized_airfoil.dat';
 [Vol_full_one_wing, ~, ~] = calculate_wing_tank_volume(c_r, c_k, c_t, b_k, b, filename, spar_locs(1), spar_locs(2));
 
 
-V_total = Vol_full_one_wing * 2 * span_fraction;
+V_total = 2*Vol_full_one_wing;
 
 % Calculate fuel mass and weight
 m_fuel = rho_fuel * V_total * 0.85; % 0.85 = volume utilization factor
